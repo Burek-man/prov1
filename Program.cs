d60a95fb6ccba872d35d10f9bb5302f92e7ff1a1
Burek-man <Alnu07001@utb.vaxjo.se>
2024-10-04 13:58:19 +0200

    prov uppgift

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+//uppgift 1
+void skrivutantal( int mat, int antal, int vikt) {
+
+}
+string mat = "burek";
+int antal = 1;
+double vikt = 3.6;
+Console.WriteLine($"jag har {antal} {mat} som väger {vikt} kg" );
+*/
+
+/* 
+//uppgift 2
+
+void SkrivUtSumma(int tal1, int tal2) {
+    int summa= tal1 + tal2;
+    Console.WriteLine($"Summan av {tal1} och {tal2} är {summa}");
+}
+
+SkrivUtSumma(4,21);
+*/
+
+
+/* 
+//uppgift 3
+void radieförcirkel(int radie){
+Console.WriteLine(radie*radie*Math.PI);
+}
+
+radieförcirkel(5);
+*/
